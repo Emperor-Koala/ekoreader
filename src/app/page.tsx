9e393cd1b1b488fd8581b244cc29516d357eb100
Matthew Christensen <christensen.developer@gmail.com>
2025-11-23 19:28:10 -0700

    setup shadcn button and dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,23 @@ import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/better-auth";
 import { getSession } from "~/server/better-auth/server";
 import { api, HydrateClient } from "~/trpc/server";
+import { Button } from "~/components/ui/button";
+import { Dialog, DialogTrigger, DialogContent } from "~/components/ui/dialog";
 
 export default async function Home() {
 	const books = await api.books.listAll();
 
 	return (
 		<HydrateClient>
-			<main className="flex min-h-screen flex-col items-center justify-center">
+			<main className="flex min-h-screen flex-col">
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button>Add Book</Button>
+					</DialogTrigger>
+					<DialogContent>
+						{/* <CreateBookForm /> */}
+					</DialogContent>
+				</Dialog>
 				{books.map((book) => <p>{book.title}</p>)}
 			</main>
 		</HydrateClient>
