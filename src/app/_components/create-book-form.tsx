0266a442dfabf04274d1b0b03d7abc7af5391eeb
Matthew Christensen <christensen.developer@gmail.com>
2025-11-23 20:50:29 -0700

    add the rest of the fields

--- src/app/_components/create-book-form.tsx
+++ src/app/_components/create-book-form.tsx
@@ -4,12 +4,17 @@ import { useForm } from "@tanstack/react-form";
 import { api } from "~/trpc/react";
 import { Field, FieldError, FieldLabel } from "~/components/ui/field";
 import { Input } from "~/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
+import { Button } from "~/components/ui/button";
+import { Calendar } from "~/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
 
 interface CreateBookFormValues {
     title: string;
     summary: string;
     authors: string[];
-    releaseDate: Date | null;
+    releaseDate: Date | undefined;
     tags: string[];
     bookFile: File | null;
 }
@@ -21,11 +26,11 @@ export const CreateBookForm = () => {
         defaultValues: {
             title: "",
             summary: "",
-            authors: [],
-            releaseDate: null,
-            tags: [],
+            authors: [""],
+            releaseDate: undefined,
+            tags: [""],
             bookFile: null,
-        } satisfies CreateBookFormValues,
+        } satisfies CreateBookFormValues as CreateBookFormValues,
 
         onSubmit: async ({ value }) => {
             console.debug(value);
@@ -33,25 +38,169 @@ export const CreateBookForm = () => {
     });
 
     return (
-        <form>
+        <form className="flex flex-col gap-5">
             <form.Field name="title">
                 {(field) => {
                     const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid
+                        field.state.meta.isTouched && !field.state.meta.isValid
                     return (
-                    <Field data-invalid={isInvalid}>
-                        <FieldLabel htmlFor={field.name}>Book Title</FieldLabel>
-                        <Input
-                            id={field.name}
-                            name={field.name}
-                            value={field.state.value}
-                            onBlur={field.handleBlur}
-                            onChange={(e) => field.handleChange(e.target.value)}
-                            aria-invalid={isInvalid}
-                            autoComplete="off"
-                        />
-                        {isInvalid && <FieldError errors={field.state.meta.errors} />}
-                    </Field>
+                        <Field data-invalid={isInvalid}>
+                            <FieldLabel htmlFor={field.name}>Book Title</FieldLabel>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => field.handleChange(e.target.value)}
+                                aria-invalid={isInvalid}
+                                autoComplete="off"
+                            />
+                            {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                        </Field>
+                    )
+                }}
+            </form.Field>
+            <form.Field name="summary">
+                {(field) => {
+                    const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                        <Field data-invalid={isInvalid}>
+                            <FieldLabel htmlFor={field.name}>Book Summary</FieldLabel>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => field.handleChange(e.target.value)}
+                                aria-invalid={isInvalid}
+                                autoComplete="off"
+                            />
+                            {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                        </Field>
+                    )
+                }}
+            </form.Field>
+            <form.Field name="releaseDate">
+                {(field) => {
+                    const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid;
+                    const date = field.state.value; 
+                    return (
+                        <Field data-invalid={isInvalid}>
+                            <FieldLabel htmlFor={field.name}>Release Date</FieldLabel>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant="outline"
+                                        data-empty={!date}
+                                        className="data-[empty=true]:text-muted-foreground w-[280px] justify-start text-left font-normal"
+                                    >
+                                    <CalendarIcon />
+                                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0">
+                                    <Calendar mode="single" selected={date} onSelect={(selected) => field.handleChange(selected)} />
+                                </PopoverContent>
+                            </Popover>
+                            {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                        </Field>
+                    )
+                }}
+            </form.Field>
+            <form.Field name="authors">
+                {(field) => {
+                    const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                        <Field data-invalid={isInvalid}>
+                            <div className="flex flex-row gap-2">
+                                <FieldLabel htmlFor={field.name}>Authors</FieldLabel>
+                                <Button 
+                                    size="sm"
+                                    className="h-6"
+                                    onClick={() => field.pushValue("")}
+                                >
+                                    + Add
+                                </Button>
+                            </div>
+                            {field.state.value.map((_, i) => (
+                                <form.Field key={`authors[${i}]`} name={`authors[${i}]`}>
+                                    {(subField) => (
+                                        <Input
+                                            id={subField.name}
+                                            name={subField.name}
+                                            value={subField.state.value}
+                                            onBlur={subField.handleBlur}
+                                            onChange={(e) => subField.handleChange(e.target.value)}
+                                            aria-invalid={isInvalid}
+                                            autoComplete="off"
+                                        />
+                                    )}
+                                </form.Field>
+                            ))}
+                            {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                        </Field>
+                    )
+                }}
+            </form.Field>
+            <form.Field name="tags">
+                {(field) => {
+                    const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                        <Field data-invalid={isInvalid}>
+                            <div className="flex flex-row gap-2">
+                                <FieldLabel htmlFor={field.name}>Tags</FieldLabel>
+                                <Button 
+                                    size="sm"
+                                    className="h-6"
+                                    onClick={() => field.pushValue("")}
+                                >
+                                    + Add
+                                </Button>
+                            </div>
+                            {field.state.value.map((_, i) => (
+                                <form.Field key={`tags[${i}]`} name={`tags[${i}]`}>
+                                    {(subField) => (
+                                        <Input
+                                            id={subField.name}
+                                            name={subField.name}
+                                            value={subField.state.value}
+                                            onBlur={subField.handleBlur}
+                                            onChange={(e) => subField.handleChange(e.target.value)}
+                                            aria-invalid={isInvalid}
+                                            autoComplete="off"
+                                        />
+                                    )}
+                                </form.Field>
+                            ))}
+                            {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                        </Field>
+                    )
+                }}
+            </form.Field>
+            <form.Field name="bookFile">
+                {(field) => {
+                    const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                        <Field data-invalid={isInvalid}>
+                            <FieldLabel htmlFor={field.name}>File</FieldLabel>
+                             <Input
+                                id={field.name}
+                                type="file"
+                                name={field.name}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => {
+                                    if (e.target.files?.length) field.handleChange(e.target.files.item(0))
+                                }}
+                                aria-invalid={isInvalid}
+                                autoComplete="off"
+                            />
+                            {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                        </Field>
                     )
                 }}
             </form.Field>
